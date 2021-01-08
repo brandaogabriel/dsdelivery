947e23aac5fbe3a51e51e746caabb57488c1a7a1
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-08 11:16:16 -0300

    Search order location

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,32 +1,33 @@
-import { ReactComponent as MainImage } from './main.svg';
+import { ReactComponent as MainImage } from "./main.svg";
 
-import './styles.css';
-import { Link } from 'react-router-dom';
-import Footer from '../Footer';
+import "./styles.css";
+import { Link } from "react-router-dom";
+import Footer from "../Footer";
 
 function Home() {
   return (
-   <>
-    <div className="home-container">
-      <div className="home-content">
-        <div className="home-actions">
-          <h1 className="home-title">
-            Faça seu pedido <br/> que entregamos <br/> pra você!
-          </h1>
-          <h3 className="home-subtitle">
-          Escolha o seu pedido e em poucos minutos <br/> levaremos na sua porta
-          </h3>
-          <Link to="/orders" className="home-btn-order">
-            FAZER PEDIDO
-          </Link>
-        </div>
-        <div className="home-image">
-          <MainImage />
+    <>
+      <div className="home-container">
+        <div className="home-content">
+          <div className="home-actions">
+            <h1 className="home-title">
+              Faça seu pedido <br /> que entregamos <br /> pra você!
+            </h1>
+            <h3 className="home-subtitle">
+              Escolha o seu pedido e em poucos minutos <br /> levaremos na sua
+              porta
+            </h3>
+            <Link to="/orders" className="home-btn-order">
+              FAZER PEDIDO
+            </Link>
+          </div>
+          <div className="home-image">
+            <MainImage />
+          </div>
         </div>
       </div>
-    </div>
-    <Footer />
-   </>
+      <Footer />
+    </>
   );
 }
 
